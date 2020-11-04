b169d72de470f2f4e05541e2833b9545611e6676
SlyWeirdo21 <wownsdl0129@gmail.com>
2020-11-04 14:41:47 +0900

    js intro 코드 추가함

---
+++ js/test-3.js
@@ -0,0 +1,12 @@
+/* function showMessage() {
+    alert("안녕하세요");
+} */
+
+/* function addToNumbers(a, b) {
+    return a + b;
+} */
+
+/*let sayHi = function() {
+    alert( "Hello" );
+  };
+alert( sayHi ); */
